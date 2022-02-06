27e3425d06dc8404acb039c4819a55970b42cc76
Abhinav Kumar <64619611+akumar2709@users.noreply.github.com>
2022-02-06 00:19:25 -0600

    Update resnet_splt.py

--- pytorch_split/resnet_splt.py
+++ pytorch_split/resnet_splt.py
@@ -47,3 +47,28 @@ resnet_split = split_model("layer3")
 
 torch.save(resnet_split[0],'resnet_split_1.pt')
 torch.save(resnet_split[1],'resnet_split_2.pt')
+
+x = torch.randn(1, 3, 224, 224)
+out = resnet_split[0](x)
+
+
+torch.onnx.export(resnet_split[0],               # model being run
+                  x,                         # model input (or a tuple for multiple inputs)
+                  "split1.onnx",   # where to save the model (can be a file or file-like object)
+                  export_params=True,        # store the trained parameter weights inside the model file
+                  opset_version=10,          # the ONNX version to export the model to
+                  do_constant_folding=True,  # whether to execute constant folding for optimization
+                  input_names = ['input'],   # the model's input names
+                  output_names = ['output'], # the model's output names
+                  dynamic_axes={'input' : {0 : 'batch_size'},    # variable length axes
+                                'output' : {0 : 'batch_size'}})
+torch.onnx.export(resnet_split[1],               # model being run
+                  out,                         # model input (or a tuple for multiple inputs)
+                  "split2.onnx",   # where to save the model (can be a file or file-like object)
+                  export_params=True,        # store the trained parameter weights inside the model file
+                  opset_version=10,          # the ONNX version to export the model to
+                  do_constant_folding=True,  # whether to execute constant folding for optimization
+                  input_names = ['input'],   # the model's input names
+                  output_names = ['output'], # the model's output names
+                  dynamic_axes={'input' : {0 : 'batch_size'},    # variable length axes
+                                'output' : {0 : 'batch_size'}})
